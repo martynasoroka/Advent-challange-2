8c386c9296c4a2fdaa2b3feac37824163e79f94f
Martyna <soroka@uxf.cz>
2023-12-16 18:53:55 +0100

    v1.0

---
+++ detail.js
@@ -0,0 +1,438 @@
+
+const movies = [
+    {
+      "adult": false,
+      "backdrop_path": "/tLsc8SCFO0rMVgVyNm9XtfnyX84.jpg",
+      "id": 726209,
+      "title": "Leave the World Behind",
+      "original_language": "en",
+      "original_title": "Leave the World Behind",
+      "overview": "A family's getaway to a luxurious rental home takes an ominous turn when a cyberattack knocks out their devices — and two strangers appear at their door.",
+      "poster_path": "/l2bqoY9rgPAgugPkOTowIPIv61j.jpg",
+      "media_type": "movie",
+      "genre_ids": [
+        18,
+        9648,
+        53,
+        878
+      ],
+      "popularity": 731.384,
+      "release_date": "2023-11-22",
+      "video": false,
+      "vote_average": 6.713,
+      "vote_count": 563
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/wVJG3u5Ru9tInxYiTCrJr4MpJ7Z.jpg",
+      "id": 1160164,
+      "title": "TAYLOR SWIFT | THE ERAS TOUR",
+      "original_language": "en",
+      "original_title": "TAYLOR SWIFT | THE ERAS TOUR",
+      "overview": "The cultural phenomenon continues on the big screen! Immerse yourself in this once-in-a-lifetime concert film experience with a breathtaking, cinematic view of the history-making tour.",
+      "poster_path": "/a5EreVlyB9fXzZ2Rf9ugOLrW5YI.jpg",
+      "media_type": "movie",
+      "genre_ids": [
+        10402
+      ],
+      "popularity": 67.016,
+      "release_date": "2023-10-13",
+      "video": false,
+      "vote_average": 8.2,
+      "vote_count": 82
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/1X7vow16X7CnCoexXh4H4F2yDJv.jpg",
+      "id": 466420,
+      "title": "Killers of the Flower Moon",
+      "original_language": "en",
+      "original_title": "Killers of the Flower Moon",
+      "overview": "When oil is discovered in 1920s Oklahoma under Osage Nation land, the Osage people are murdered one by one—until the FBI steps in to unravel the mystery.",
+      "poster_path": "/dB6Krk806zeqd0YNp2ngQ9zXteH.jpg",
+      "media_type": "movie",
+      "genre_ids": [
+        80,
+        18,
+        36
+      ],
+      "popularity": 2057.537,
+      "release_date": "2023-10-18",
+      "video": false,
+      "vote_average": 7.663,
+      "vote_count": 1344
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/yOm993lsJyPmBodlYjgpPwBjXP9.jpg",
+      "id": 787699,
+      "title": "Wonka",
+      "original_language": "en",
+      "original_title": "Wonka",
+      "overview": "Willy Wonka – chock-full of ideas and determined to change the world one delectable bite at a time – is proof that the best things in life begin with a dream, and if you’re lucky enough to meet Willy Wonka, anything is possible.",
+      "poster_path": "/qhb1qOilapbapxWQn9jtRCMwXJF.jpg",
+      "media_type": "movie",
+      "genre_ids": [
+        35,
+        10751,
+        14
+      ],
+      "popularity": 1117.04,
+      "release_date": "2023-12-06",
+      "video": false,
+      "vote_average": 7.13,
+      "vote_count": 115
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/8GnWDLn2AhnmkQ7hlQ9NJUYobSS.jpg",
+      "id": 695721,
+      "title": "The Hunger Games: The Ballad of Songbirds & Snakes",
+      "original_language": "en",
+      "original_title": "The Hunger Games: The Ballad of Songbirds & Snakes",
+      "overview": "64 years before he becomes the tyrannical president of Panem, Coriolanus Snow sees a chance for a change in fortunes when he mentors Lucy Gray Baird, the female tribute from District 12.",
+      "poster_path": "/mBaXZ95R2OxueZhvQbcEWy2DqyO.jpg",
+      "media_type": "movie",
+      "genre_ids": [
+        28,
+        10749,
+        18
+      ],
+      "popularity": 619.834,
+      "release_date": "2023-11-15",
+      "video": false,
+      "vote_average": 7.248,
+      "vote_count": 689
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/fm6KqXpk3M2HVveHwCrBSSBaO0V.jpg",
+      "id": 872585,
+      "title": "Oppenheimer",
+      "original_language": "en",
+      "original_title": "Oppenheimer",
+      "overview": "The story of J. Robert Oppenheimer's role in the development of the atomic bomb during World War II.",
+      "poster_path": "/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
+      "media_type": "movie",
+      "genre_ids": [
+        18,
+        36
+      ],
+      "popularity": 898.326,
+      "release_date": "2023-07-19",
+      "video": false,
+      "vote_average": 8.136,
+      "vote_count": 5369
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/feSiISwgEpVzR1v3zv2n2AU4ANJ.jpg",
+      "id": 609681,
+      "title": "The Marvels",
+      "original_language": "en",
+      "original_title": "The Marvels",
+      "overview": "Carol Danvers, aka Captain Marvel, has reclaimed her identity from the tyrannical Kree and taken revenge on the Supreme Intelligence. But unintended consequences see Carol shouldering the burden of a destabilized universe. When her duties send her to an anomalous wormhole linked to a Kree revolutionary, her powers become entangled with that of Jersey City super-fan Kamala Khan, aka Ms. Marvel, and Carol’s estranged niece, now S.A.B.E.R. astronaut Captain Monica Rambeau. Together, this unlikely trio must team up and learn to work in concert to save the universe.",
+      "poster_path": "/Ag3D9qXjhJ2FUkrlJ0Cv1pgxqYQ.jpg",
+      "media_type": "movie",
+      "genre_ids": [
+        878,
+        12,
+        28
+      ],
+      "popularity": 499.118,
+      "release_date": "2023-11-08",
+      "video": false,
+      "vote_average": 6.549,
+      "vote_count": 673
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/a21obwG9qMSf1qa6oG4ZgpOyMaA.jpg",
+      "id": 870358,
+      "title": "Merry Little Batman",
+      "original_language": "en",
+      "original_title": "Merry Little Batman",
+      "overview": "This Christmas, Damian Wayne wants to be a superhero like his dad – the one and only Batman. When Damian is left home alone while Batman takes on Gotham’s worst supervillains on Christmas Eve, he stumbles upon a villainous plot to steal Christmas and leaps at the chance to save the day.",
+      "poster_path": "/2J70guF8wcErbYbRgZUBcvShgI1.jpg",
+      "media_type": "movie",
+      "genre_ids": [
+        16,
+        28,
+        10751,
+        35
+      ],
+      "popularity": 201.033,
+      "release_date": "2023-12-08",
+      "video": false,
+      "vote_average": 6.7,
+      "vote_count": 39
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/kjQBrc00fB2RjHZB3PGR4w9ibpz.jpg",
+      "id": 670292,
+      "title": "The Creator",
+      "original_language": "en",
+      "original_title": "The Creator",
+      "overview": "Amid a future war between the human race and the forces of artificial intelligence, a hardened ex-special forces agent grieving the disappearance of his wife, is recruited to hunt down and kill the Creator, the elusive architect of advanced AI who has developed a mysterious weapon with the power to end the war—and mankind itself.",
+      "poster_path": "/vBZ0qvaRxqEhZwl6LWmruJqWE8Z.jpg",
+      "media_type": "movie",
+      "genre_ids": [
+        28,
+        12,
+        18
+      ],
+      "popularity": 606.742,
+      "release_date": "2023-09-27",
+      "video": false,
+      "vote_average": 7.112,
+      "vote_count": 1411
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/aUfAbn4WXkioO5FPKAyxnVGaVqg.jpg",
+      "id": 1023845,
+      "title": "Silver and the Book of Dreams",
+      "original_language": "de",
+      "original_title": "Silber und das Buch der Träume",
+      "overview": "When Liv moves to London, she meets the mysterious Henry and his group of friends, who pull her into the enthralling world of dream-travel. She and her newfound band of dream-wanderers conduct a ritual to make their biggest dream come true, but it demands the gravest sacrifice.",
+      "poster_path": "/ycpxvpNyrkzsydwvLTUNixRWrdQ.jpg",
+      "media_type": "movie",
+      "genre_ids": [
+        14,
+        878,
+        10749
+      ],
+      "popularity": 27.138,
+      "release_date": "2023-11-06",
+      "video": false,
+      "vote_average": 6.292,
+      "vote_count": 36
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/bckxSN9ueOgm0gJpVJmPQrecWul.jpg",
+      "id": 572802,
+      "title": "Aquaman and the Lost Kingdom",
+      "original_language": "en",
+      "original_title": "Aquaman and the Lost Kingdom",
+      "overview": "Black Manta, still driven by the need to avenge his father's death and wielding the power of the mythic Black Trident, will stop at nothing to take Aquaman down once and for all. To defeat him, Aquaman must turn to his imprisoned brother Orm, the former King of Atlantis, to forge an unlikely alliance in order to save the world from irreversible destruction.",
+      "poster_path": "/8xV47NDrjdZDpkVcCFqkdHa3T0C.jpg",
+      "media_type": "movie",
+      "genre_ids": [
+        28,
+        12,
+        14
+      ],
+      "popularity": 328.539,
+      "release_date": "2023-12-20",
+      "video": false,
+      "vote_average": 0,
+      "vote_count": 0
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/kG8iWrKGNlrUM7LfT6zJEOXglZd.jpg",
+      "id": 945937,
+      "title": "Fast Charlie",
+      "original_language": "en",
+      "original_title": "Fast Charlie",
+      "overview": "When his aging mob boss is whacked, Charlie Swift, a loyal friend and hired gun, will stop at nothing to destroy the upcoming crew that took him out.",
+      "poster_path": "/3y72ffwYRUPOj4yOQbiTaN897Tm.jpg",
+      "media_type": "movie",
+      "genre_ids": [
+        28,
+        80,
+        18
+      ],
+      "popularity": 237.315,
+      "release_date": "2023-12-08",
+      "video": false,
+      "vote_average": 6.3,
+      "vote_count": 15
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/t5zCBSB5xMDKcDqe91qahCOUYVV.jpg",
+      "id": 507089,
+      "title": "Five Nights at Freddy's",
+      "original_language": "en",
+      "original_title": "Five Nights at Freddy's",
+      "overview": "Recently fired and desperate for work, a troubled young man named Mike agrees to take a position as a night security guard at an abandoned theme restaurant: Freddy Fazbear's Pizzeria. But he soon discovers that nothing at Freddy's is what it seems.",
+      "poster_path": "/7BpNtNfxuocYEVREzVMO75hso1l.jpg",
+      "media_type": "movie",
+      "genre_ids": [
+        27,
+        9648
+      ],
+      "popularity": 662.586,
+      "release_date": "2023-10-25",
+      "video": false,
+      "vote_average": 7.836,
+      "vote_count": 2783
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/ctMserH8g2SeOAnCw5gFjdQF8mo.jpg",
+      "id": 346698,
+      "title": "Barbie",
+      "original_language": "en",
+      "original_title": "Barbie",
+      "overview": "Barbie and Ken are having the time of their lives in the colorful and seemingly perfect world of Barbie Land. However, when they get a chance to go to the real world, they soon discover the joys and perils of living among humans.",
+      "poster_path": "/iuFNMS8U5cb6xfzi51Dbkovj7vM.jpg",
+      "media_type": "movie",
+      "genre_ids": [
+        35,
+        12,
+        14
+      ],
+      "popularity": 387.458,
+      "release_date": "2023-07-19",
+      "video": false,
+      "vote_average": 7.176,
+      "vote_count": 6244
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/bWIIWhnaoWx3FTVXv6GkYDv3djL.jpg",
+      "id": 940721,
+      "title": "Godzilla Minus One",
+      "original_language": "ja",
+      "original_title": "ゴジラ-1.0",
+      "overview": "In postwar Japan, a new terror rises. Will the devastated people be able to survive... let alone fight back?",
+      "poster_path": "/hkxxMIGaiCTmrEArK7J56JTKUlB.jpg",
+      "media_type": "movie",
+      "genre_ids": [
+        878,
+        27,
+        28
+      ],
+      "popularity": 432.589,
+      "release_date": "2023-11-03",
+      "video": false,
+      "vote_average": 8.302,
+      "vote_count": 111
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/aEgUv1KzUGPleCUuHrFVNvsyBUx.jpg",
+      "id": 1123093,
+      "title": "Diary of a Wimpy Kid Christmas: Cabin Fever",
+      "original_language": "en",
+      "original_title": "Diary of a Wimpy Kid Christmas: Cabin Fever",
+      "overview": "The winter holidays are turning out to be especially stressful for Greg Heffley this year. After accidentally damaging a snowplow while making a snowman with best friend Rowley Jefferson, Greg worries he won’t get the new video game console he so desperately wants for Christmas. To make matters worse, he gets snowed in with his family, including his grumpy older brother Rodrick and annoying younger brother Manny.",
+      "poster_path": "/tV0996od52EJ6S8dLKvcVGsO7B.jpg",
+      "media_type": "movie",
+      "genre_ids": [
+        16,
+        35,
+        10751
+      ],
+      "popularity": 224.434,
+      "release_date": "2023-12-08",
+      "video": false,
+      "vote_average": 6.3,
+      "vote_count": 12
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/35z8hWuzfFUZQaYog8E9LsXW3iI.jpg",
+      "id": 335977,
+      "title": "Indiana Jones and the Dial of Destiny",
+      "original_language": "en",
+      "original_title": "Indiana Jones and the Dial of Destiny",
+      "overview": "Finding himself in a new era, and approaching retirement, Indy wrestles with fitting into a world that seems to have outgrown him. But as the tentacles of an all-too-familiar evil return in the form of an old rival, Indy must don his hat and pick up his whip once more to make sure an ancient and powerful artifact doesn't fall into the wrong hands.",
+      "poster_path": "/Af4bXE63pVsb2FtbW8uYIyPBadD.jpg",
+      "media_type": "movie",
+      "genre_ids": [
+        12,
+        28
+      ],
+      "popularity": 279.593,
+      "release_date": "2023-06-28",
+      "video": false,
+      "vote_average": 6.692,
+      "vote_count": 2221
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/fCwwzOziFYs7YRwP4gbrb9qH1xg.jpg",
+      "id": 1011985,
+      "title": "Kung Fu Panda 4",
+      "original_language": "en",
+      "original_title": "Kung Fu Panda 4",
+      "overview": "Po is gearing up to become the spiritual leader of his Valley of Peace, but also needs someone to take his place as Dragon Warrior. As such, he will train a new kung fu practitioner for the spot and will encounter a villain called the Chameleon who conjures villains from the past.",
+      "poster_path": "/aOuX4hhYm0vbHP7dR2JLk6BAcfn.jpg",
+      "media_type": "movie",
+      "genre_ids": [
+        28,
+        12,
+        16,
+        35,
+        10751
+      ],
+      "popularity": 34.979,
+      "release_date": "2024-03-07",
+      "video": false,
+      "vote_average": 0,
+      "vote_count": 0
+    },
+  ]
+
+
+  const detail = document.querySelector(".movie-detail")
+
+  detail.innerHTML = ""
+
+  movies.forEach((key, index) => {
+    if (movies[index].id === location.hash.slice(1)) {
+        detail.innerHTML = `
+					<div class="poster">
+						<img
+							src="https://www.themoviedb.org/t/p/original${movies[index].title}"
+							alt="${movies[index].title}"
+                            width="663"
+						/>
+					</div>
+					<div class="card-body">
+							<h5 class="card-title">${movies[index].title}</h5>
+							<p class="card-text">${movies[index].overview}</p>
+							<p class="card-text">
+								<small id="premiera"
+									>Release date: <strong>${dayjs(movies[index].release_date).format("D-M-YYYY")}</strong>
+							</p>
+                            <div class="back">
+                                <a href="index.html">
+                                <i class="fa fa-arrow-circle-o-left" style="font-size:36px"></i>
+                                </a>
+                                <p>Back to the list</p>
+                            </div>
+					</div>
+        `
+
+    }
+})
+
+  /*
+
+          detail.innerHTML = `<h1>LOL</h1>`
+        <div id="movie-detail">
+					<div class="poster">
+						<img
+							src="https://via.placeholder.com/663x909?text=Plak%C3%A1t"
+							alt="plakát"
+							width="663"
+							height="909"
+						/>
+					</div>
+					<div class="card-body">
+							<h5 class="card-title">${movies[index].title}</h5>
+							<p class="card-text">Tady bude dluhý popis filmu.</p>
+							<p class="card-text">
+								<small id="premiera"
+									>Premiéra <strong>24. prosince 2022</strong>
+							</p>
+					</div>
+			</div>
+    if (movies[index].id === location.hash.slice(1)) {
+  */
